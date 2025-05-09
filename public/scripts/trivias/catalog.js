b5b01d0e7eed429f26cd9757e7163b42327cb768
bishusom <bishusom@gmail.com>
2025-05-09 21:01:50 +0800

    That's it, I am done for today !!

--- public/scripts/trivias/catalog.js
+++ public/scripts/trivias/catalog.js
@@ -3,7 +3,6 @@ export function initTriviaCatalog() {
     setupCarousel();
     setupCategoryCards();
     updateProgressTracker();
-    setupEvents();
 }
 
 // Add smooth scrolling behavior for mobile
